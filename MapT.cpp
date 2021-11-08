0242d08b5c64baa6ac9ba79b43e4e3764403c0d5
DrErickson <77122859+DrErickson@users.noreply.github.com>
2021-11-08 09:13:52 -0800

    Initial commit

---
+++ MapT.cpp
@@ -0,0 +1,21 @@
+//
+// Created by we7289 on 4/19/2021.
+//
+
+#include "MapT.h"
+#include <unordered_map>
+//using namespace std;
+
+const int DEFAULT_BUCKETS = 10;
+const double DEFAULT_LOAD = 1.0;
+
+
+template<class K, class T>
+int MapT<K, T>::GetHashIndex(const K &key) {
+    unordered_map<K,T> mapper;
+    typename unordered_map<K,T>::hasher hashFunction = mapper.hash_function();
+    return static_cast<int>(hashFunction(key) % numBuckets);
+}
+
+
+
